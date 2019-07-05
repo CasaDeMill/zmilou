0d4ad8c159f84bb9484bbdba45d49aeca9e849eb
Martin <artinmall96@gmail.com>
2019-07-05 16:42:34 +0200

    Dog

--- src/App.js
+++ src/App.js
@@ -70,19 +70,24 @@ import './index.css'
 // }
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
-const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
-const trans2 = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`
-const trans3 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`
-const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`
+const trans1 = (x) => `translate3d(0,0,0)`
+const trans2 = (x, y) => `translate3d(${x / 3 + 35}px,${y / 15 - 230}px,0)`
+// const trans3 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`
+// const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`
 
 function Card() {
   const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
   return (
     <div class="container" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+      <link 
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Underdog">
+      </link>
+      <h1 className="title">CasaDeMill</h1>
       <animated.div class="card1" style={{ transform: props.xy.interpolate(trans1) }} />
       <animated.div class="card2" style={{ transform: props.xy.interpolate(trans2) }} />
-      <animated.div class="card3" style={{ transform: props.xy.interpolate(trans3) }} />
-      <animated.div class="card4" style={{ transform: props.xy.interpolate(trans4) }} />
+      {/* <animated.div class="card3" style={{ transform: props.xy.interpolate(trans3) }} />
+      <animated.div class="card4" style={{ transform: props.xy.interpolate(trans4) }} /> */}
     </div>
   )
 }
